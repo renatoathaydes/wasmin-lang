0cd4d9c6a5f4ccb79cdcfddaae89931a12b9cc05
Renato Athaydes <renato@athaydes.com>
2020-11-27 20:10:56 +0100

    Handle whitespaces and words properly.

--- src/parse.rs
+++ src/parse.rs
@@ -5,9 +5,21 @@ use crate::ast::Expression;
 use crate::types::{*, Type::*};
 use crate::types::type_of;
 
+/// space_or takes a char and two expressions.
+/// The first expression is evaluated if the char is whitespace.
+/// The second expression is evaluated otherwise.
+macro_rules! space_or {
+    ( $c:expr, $space:expr, $other:expr ) => {
+        match $c {
+            ' ' | '\n' | '\t' | '\r' => $space,
+            _ => $other
+        }
+    };
+}
+
 pub struct Parser<'s> {
-    line: u32,
-    col: u32,
+    line: usize,
+    col: usize,
     symbols: HashMap<String, Type>,
     chars: &'s mut Chars<'s>,
     curr_char: Option<char>,
@@ -21,7 +33,12 @@ impl Parser<'_> {
     fn next(&mut self) -> Option<char> {
         let next = self.chars.next();
         if let Some(c) = next {
-            if c == '\n' { self.line += 1; } else { self.col += 1; }
+            if c == '\n' {
+                self.line += 1;
+                self.col = 0;
+            } else {
+                self.col += 1;
+            }
         }
         self.curr_char = next;
         next
@@ -40,36 +57,31 @@ impl Parser<'_> {
     //     }
     // }
 
-    fn parse_word(&mut self) -> String {
+    fn pos(&mut self) -> (usize, usize) {
+        (self.line, self.col)
+    }
+
+    fn parse_word(&mut self) -> Option<String> {
         self.skip_spaces();
         let c = self.curr_char;
-        if c.is_none() { return "".to_string(); }
+        if c.is_none() { return None; }
         let mut word = String::with_capacity(8);
         word.push(c.unwrap());
         loop {
             if let Some(c) = self.next() {
-                match c {
-                    ' ' | '\n' | '\t' => { return word; }
-                    _ => { word.push(c) }
-                }
+                space_or!(c, { break; }, { word.push(c) });
             } else { break; }
         }
-        word
+        Some(word)
     }
 
     fn skip_spaces(&mut self) {
         if let Some(c) = self.curr_char {
-            match c {
-                ' ' | '\n' | '\t' => {}
-                _ => { return; }
-            }
+            space_or!(c, {}, { return; });
         }
         loop {
             if let Some(c) = self.next() {
-                match c {
-                    ' ' | '\n' | '\t' => { /* continue */ }
-                    _ => break
-                }
+                space_or!(c, {}, { break; });
             } else { break; }
         }
     }
@@ -143,6 +155,42 @@ mod tests {
     fn test_word() {
         let mut chars = "abc".chars();
         let mut parser = new_parser(&mut chars);
-        assert_eq!(parser.parse_word(), "abc");
+        assert_eq!(parser.parse_word(), Some("abc".to_string()));
+    }
+
+    #[test]
+    fn test_words() {
+        let mut chars = "a b  cde    fgh\nij\n  klmnop  \r\n  rs  ".chars();
+        let mut parser = new_parser(&mut chars);
+        assert_eq!(parser.parse_word(), Some("a".to_string()));
+        assert_eq!(parser.parse_word(), Some("b".to_string()));
+        assert_eq!(parser.parse_word(), Some("cde".to_string()));
+        assert_eq!(parser.parse_word(), Some("fgh".to_string()));
+        assert_eq!(parser.parse_word(), Some("ij".to_string()));
+        assert_eq!(parser.parse_word(), Some("klmnop".to_string()));
+        assert_eq!(parser.parse_word(), Some("rs".to_string()));
+        assert_eq!(parser.parse_word(), None);
+        assert_eq!(parser.parse_word(), None);
+    }
+
+    #[test]
+    fn test_pos() {
+        let mut chars = "a b  cde    fgh\nij\n  klmnop  \r\n  rs  ".chars();
+        let mut parser = new_parser(&mut chars);
+        assert_eq!(parser.pos(), (0, 0));
+        parser.parse_word();
+        assert_eq!(parser.pos(), (0, 2));
+        parser.parse_word();
+        assert_eq!(parser.pos(), (0, 4));
+        parser.parse_word();
+        assert_eq!(parser.pos(), (0, 9));
+        parser.parse_word();
+        assert_eq!(parser.pos(), (1, 0));
+        parser.parse_word();
+        assert_eq!(parser.pos(), (2, 0));
+        parser.parse_word();
+        assert_eq!(parser.pos(), (2, 9));
+        parser.parse_word();
+        assert_eq!(parser.pos(), (3, 5));
     }
 }
