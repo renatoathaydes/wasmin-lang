f9c629ea3b27123671f20c13608934ff683ea596
Renato Athaydes <renato@athaydes.com>
2021-02-03 19:44:02 +0100

    Added remaining WASM operators.

--- src/wasm_funs.rs
+++ src/wasm_funs.rs
@@ -25,7 +25,7 @@ pub fn wasm_std_funs() -> Stack {
     });
 
     [I32, I64].iter().for_each(|t| {
-        ["clz", "ctz", "popcnt", "eqz"].iter().for_each(|name| {
+        ["clz", "ctz", "popcnt", "eqz", "extend_8s", "extend_16s"].iter().for_each(|name| {
             stack.push(name.to_owned().to_owned(), Type::WasmFn(vec![fun_type!([t.clone()](t.clone()))]), true, false).unwrap();
         });
     });
@@ -35,6 +35,41 @@ pub fn wasm_std_funs() -> Stack {
             stack.push(name.to_owned().to_owned(), Type::WasmFn(vec![fun_type!([t.clone()](t.clone()))]), true, false).unwrap();
         });
     });
+
+    // conversion functions
+
+    [(F32, I32), (F64, I32)].for_each(|(from, to)| {
+        ["convert_i32_u", "convert_i32_s"].iter().for_each(|name| {
+            stack.push(name.to_owned().to_owned(), Type::WasmFn(vec![fun_type!([from.clone()](to.clone()))]), true, false).unwrap();
+        });
+    });
+
+    [(F32, I64), (F64, I64)].for_each(|(from, to)| {
+        ["convert_i64_u", "convert_i64_s"].iter().for_each(|name| {
+            stack.push(name.to_owned().to_owned(), Type::WasmFn(vec![fun_type!([from.clone()](to.clone()))]), true, false).unwrap();
+        });
+    });
+
+    [(F64, I32), (F64, I64)].for_each(|(from, to)| {
+        ["trunc_f64_s", "trunc_f64_u", "trunc_sat_f64_s", "trunc_sat_f64_u"].iter().for_each(|name| {
+            stack.push(name.to_owned().to_owned(), Type::WasmFn(vec![fun_type!([from.clone()](to.clone()))]), true, false).unwrap();
+        });
+    });
+
+    [(F32, I32), (F32, I64)].for_each(|(from, to)| {
+        ["trunc_sat_f32_s", "trunc_sat_f32_u"].iter().for_each(|name| {
+            stack.push(name.to_owned().to_owned(), Type::WasmFn(vec![fun_type!([from.clone()](to.clone()))]), true, false).unwrap();
+        });
+    });
+
+    stack.push("wrap_i64".to_owned(), Type::WasmFn(vec![fun_type!([I64](I32))]), true, false).unwrap();
+    stack.push("extend_32s".to_owned(), Type::WasmFn(vec![fun_type!([I64](I64))]), true, false).unwrap();
+    stack.push("demote_f64".to_owned(), Type::WasmFn(vec![fun_type!([F64](F32))]), true, false).unwrap();
+    stack.push("promote_f32".to_owned(), Type::WasmFn(vec![fun_type!([F32](F64))]), true, false).unwrap();
+    stack.push("reinterpret_i32".to_owned(), Type::WasmFn(vec![fun_type!([I32](F32))]), true, false).unwrap();
+    stack.push("reinterpret_f32".to_owned(), Type::WasmFn(vec![fun_type!([F32](I32))]), true, false).unwrap();
+    stack.push("reinterpret_i64".to_owned(), Type::WasmFn(vec![fun_type!([I64](F64))]), true, false).unwrap();
+    stack.push("reinterpret_f64".to_owned(), Type::WasmFn(vec![fun_type!([F64](I64))]), true, false).unwrap();
 
     stack
 }
